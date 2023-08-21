ed858756f850ce17c92bdf54b5473c5695f1418e
Flashlearner1 <osayandekingsley768@gmail.com>
2023-08-21 02:25:01 -0700

    FIGMA-REACT LANDING PAGE

---
+++ FIGMA-REACT/src/App.jsx
@@ -0,0 +1,142 @@
+import pic1 from './assets/pic1.jpg.png'
+import pic2 from './assets/pic2.jpg.jpg'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="index">
+      <div className="Rectangle1267">
+        <div className="Group285">
+          <div className="Group286">
+            <div className="logo">
+              <div className="logo-shape">
+                <div className="rectangle1266"></div>
+                <div className="rectangle1265"></div>
+              </div>
+              <div className="Boldo">Boldo</div>
+            </div>
+            <div className="Navigation">
+              <ul>
+                <li className="P"><a href="">Product</a></li>
+                <li className="S"><a href="">Services</a></li>
+                <li className="A"><a href="">About</a></li>
+              </ul>
+              <div className="Login-button">
+                <a href="">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="eclipse9">
+        <div className="Frame284">
+
+          <div className="Frame267">
+            <div className="Frame287">
+              <h1 className="save">Save time by building <br /> fast with Boldo Template </h1>
+              <p className="fund">Funding handshake buyer business-to-business metrics iPad partnership. First mover advantage innovator success deployment non-disclosure.</p>
+            </div>
+            <div className="Frame288">
+              <div>
+                <button className="Buy"><a href="">Buy Template</a></button>
+              </div>
+              <div>
+              <button className="Ex"><a href="">Explore</a></button>
+              </div>
+            </div>
+          </div>
+
+          <div>
+          <img className='eclipse3' src={pic1} alt="pic5" />
+          </div>
+          
+          
+        </div>
+
+        <div className="logo-wall">
+          <div className="rectangle1288"></div>
+          <div className="logos">
+            <div className="logo-1">
+              <div className="logo-shape1">
+                <div className="rectangle1234"></div>
+                <div className="rectangle1235"></div>
+              </div>
+              <div className="Text-1">Boldo</div>
+            </div>
+
+            <div className="logo-2">
+              <div className="logo-2a">
+                <div>
+                  <img className='presto' src={pic2} alt="pic5" />
+                </div>
+                <div className="Text-2">Presto</div>
+                
+              </div>
+            </div>
+
+            <div className="logo-1">
+              <div className="logo-shape1">
+                <div className="rectangle1234"></div>
+                <div className="rectangle1235"></div>
+              </div>
+              <div className="Text-1">Boldo</div>
+            </div>
+
+            <div className="logo-2">
+              <div className="logo-2a">
+                <div>
+                  <img className='presto' src={pic2} alt="pic5" />
+                </div>
+                <div className="Text-2">Presto</div>
+                
+              </div>
+            </div>
+
+            <div className="logo-1">
+              <div className="logo-shape1">
+                <div className="rectangle1234"></div>
+                <div className="rectangle1235"></div>
+              </div>
+              <div className="Text-1">Boldo</div>
+            </div>
+
+            <div className="logo-2">
+              <div className="logo-2a">
+                <div>
+                  <img className='presto' src={pic2} alt="pic5" />
+                </div>
+                <div className="Text-2">Presto</div>
+                
+              </div>
+            </div>
+
+            <div className="logo-1">
+              <div className="logo-shape1">
+                <div className="rectangle1234"></div>
+                <div className="rectangle1235"></div>
+              </div>
+              <div className="Text-1">Boldo</div>
+            </div>
+
+          </div>
+        </div>
+        
+          
+        </div>
+
+        
+        
+      </div>
+      
+      
+
+     
+    </div>
+    </>
+  )
+}
+
+export default App
